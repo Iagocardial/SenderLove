7c271421258dfce8e5e212da01febaa19c6c11da
iago cardial <iago.cardial@welbecare.com>
2024-10-20 17:02:06 -0300

    Splitting the project in 2, backEnd and frontEnd

--- backend/src/mail-templates/emailTemplates.ts
+++ backend/src/mail-templates/emailTemplates.ts
@@ -1,16 +1,12 @@
-// src/mail-templates/emailTemplates.ts
 import fs from 'fs';
 import path from 'path';
 
-// Definir o tipo da função, que recebe o nome do arquivo e retorna uma string
 const loadTemplate = (fileName: string): string => {
   const filePath = path.join(__dirname, 'templates', fileName);
   return fs.readFileSync(filePath, 'utf8');
 };
 
-// Carregar os templates
 const firstTemplateHtml: string = loadTemplate('first-html.html');
 const giftIdeas: string = loadTemplate('gifts-ideas.html');
 
-// Exportar os templates
 export { firstTemplateHtml, giftIdeas };
